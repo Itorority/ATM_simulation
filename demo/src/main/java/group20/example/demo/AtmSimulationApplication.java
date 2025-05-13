6a54b7ee7b8a8afcaaff5c4b4f61f6e3d2a03fc5
Itorority <itorority@gmail.com>
2025-05-13 10:56:42 +0700

    Update AtmSimulationApplication.java

--- demo/src/main/java/group20/example/demo/AtmSimulationApplication.java
+++ demo/src/main/java/group20/example/demo/AtmSimulationApplication.java
@@ -8,6 +8,7 @@ public class AtmSimulationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AtmSimulationApplication.class, args);
+		System.out.println("Testing 1 2 3....");
+		System.out.println("Hello 1 2 3....");
 	}
-
 }
