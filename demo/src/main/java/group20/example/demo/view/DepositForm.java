c916006e863de9cb0a1ed3ed98558bc9c51f90ce
Itorority <itorority@gmail.com>
2025-05-27 19:41:31 +0700

    Update UI

--- demo/src/main/java/group20/example/demo/view/DepositForm.java
+++ demo/src/main/java/group20/example/demo/view/DepositForm.java
@@ -51,42 +51,53 @@ public class DepositForm extends JFrame implements IForm {
 		panel.setBackground(new Color(220, 220, 220));
 		setContentPane(panel);
 
-		JPanel jpTop = new JPanel(new BorderLayout());
+		JPanel jpTop = new JPanel();
+		jpTop.setLayout(new BoxLayout(jpTop, BoxLayout.X_AXIS));
 		jpTop.setOpaque(false);
 		jpTop.setBorder(new EmptyBorder(20, 30, 20, 30));
-
+		
+		// Panel logo
 		JLabel labLogo = new JLabel("ATM Simulator");
 		labLogo.setFont(new Font("Arial", Font.BOLD, 25));
-		jpTop.add(labLogo, BorderLayout.WEST);
+		jpTop.add(labLogo);
 
+		// Panel Hotline
 		JPanel jpHotline = new JPanel();
 		jpHotline.setLayout(new BoxLayout(jpHotline, BoxLayout.Y_AXIS));
 		jpHotline.setOpaque(false);
-
 		JLabel labHot1 = new JLabel("HOTLINE ATM");
 		labHot1.setFont(new Font("Arial", Font.PLAIN, 15));
+		labHot1.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		JLabel labHot2 = new JLabel("1900 1010 - 1010 1900");
 		labHot2.setFont(new Font("Arial", Font.PLAIN, 15));
+		labHot2.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		jpHotline.add(labHot1);
 		jpHotline.add(labHot2);
-
+		jpTop.add(Box.createHorizontalGlue());
 		jpTop.add(jpHotline, BorderLayout.EAST);
+		
 		panel.add(jpTop, BorderLayout.NORTH);
 
 		// Content Panel
 		JPanel contentPanel = new JPanel();
 		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
 		contentPanel.setOpaque(false);
+		
+		JLabel titleVN = new JLabel("Nạp tiền");
+		titleVN.setFont(new Font("Arial", Font.BOLD, 38));
+		titleVN.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-		JLabel englishMessage = new JLabel("Please insert the money into the tray and confirm the deposit");
+		contentPanel.add(titleVN);
+		contentPanel.add(Box.createVerticalStrut(20)); 
+
 		JLabel vietnameseMessage = new JLabel("Quý khách vui lòng đưa tiền vào khay và xác nhận nạp tiền");
 
-		englishMessage.setFont(new Font("Arial", Font.PLAIN, 25));
-		englishMessage.setAlignmentX(Component.CENTER_ALIGNMENT);
 		vietnameseMessage.setFont(new Font("Arial", Font.PLAIN, 25));
 		vietnameseMessage.setAlignmentX(Component.CENTER_ALIGNMENT);
 
 		moneyField = new JTextField();
+		moneyField.setMaximumSize(new Dimension(400, 40)); 
+		moneyField.setPreferredSize(new Dimension(400, 40)); 
 		JLabel moneyText = new JLabel("Số tiền đã để vào khay");
 		moneyText.setFont(new Font("Arial", Font.PLAIN, 25));
 		moneyText.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -94,30 +105,33 @@ public class DepositForm extends JFrame implements IForm {
 		JButton confirmButton = new JButton("Xác nhận");
 		JButton cancelButton = new JButton("Hủy bỏ");
 
-		Dimension buttonSize = new Dimension(200, 100);
-		confirmButton.setPreferredSize(buttonSize);
-		cancelButton.setPreferredSize(buttonSize);
-		confirmButton.setMaximumSize(buttonSize);
-		cancelButton.setMaximumSize(buttonSize);
-		confirmButton.setFont(new Font("Arial", Font.BOLD, 18));
-		cancelButton.setFont(new Font("Arial", Font.BOLD, 18));
+		confirmButton.setFont(new Font("Arial", Font.BOLD, 25));
+		cancelButton.setFont(new Font("Arial", Font.BOLD, 25));
+		
+		confirmButton.setBackground(Color.BLUE);
+		confirmButton.setForeground(Color.WHITE);
+
+		cancelButton.setBackground(Color.BLUE);
+		cancelButton.setForeground(Color.WHITE);
 
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setOpaque(false);
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
 		buttonPanel.add(Box.createHorizontalGlue());
 		buttonPanel.add(cancelButton);
-		buttonPanel.add(Box.createHorizontalStrut(200));
+		buttonPanel.add(Box.createHorizontalStrut(50));
 		buttonPanel.add(confirmButton);
 		buttonPanel.add(Box.createHorizontalGlue());
 
-		contentPanel.add(Box.createVerticalStrut(20));
-		contentPanel.add(englishMessage);
+		// Thêm button panel vào content panel
+		contentPanel.add(buttonPanel);
+		contentPanel.setBorder(new EmptyBorder(30, 100, 20, 100));
+
 		contentPanel.add(vietnameseMessage);
 		contentPanel.add(Box.createVerticalStrut(30));
 		contentPanel.add(moneyText);
 		contentPanel.add(moneyField);
-		contentPanel.add(Box.createVerticalStrut(90));
+		contentPanel.add(Box.createVerticalStrut(40));
 		contentPanel.add(buttonPanel);
 
 		contentPanel.setBorder(new EmptyBorder(50, 0, 0, 0));
@@ -150,8 +164,10 @@ public class DepositForm extends JFrame implements IForm {
 		});
 
 		cancelButton.addActionListener(e -> {
-			new MainForm(context, currentUser, currentAccount).setVisible(true);
-			this.dispose();
+			MainForm mainForm = MainForm.getInstance(context, currentUser, currentAccount);
+			mainForm.setLocationRelativeTo(null);
+			mainForm.setVisible(true);
+			dispose();
 		});
 
 	}
